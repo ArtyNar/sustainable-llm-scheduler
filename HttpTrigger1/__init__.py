2b3527fc371cf5910f742b67eb008a7f171ed98b
ArtyNar <arty.nar00@gmail.com>
2025-11-24 15:53:50 -0700

    Implement timer push

--- HttpTrigger1/__init__.py
+++ HttpTrigger1/__init__.py
@@ -41,7 +41,6 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
             status_code=500,
             mimetype="application/json"
         )
-    
 
     table_name  = "prompttable"
     table_client = TableServiceClient.from_connection_string(DEPLOYMENT_STORAGE_CONNECTION_STRING).get_table_client(table_name)
@@ -50,20 +49,19 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     rows = [dict(e) for e in entities]
 
     # Lastly, store the CI in a table
-    cur_CI, cur_zone, timestamp = get_cur_CI(ELECTRICITY_MAPS_API_KEY)
+    # cur_CI, cur_zone, timestamp = get_cur_CI(ELECTRICITY_MAPS_API_KEY)
 
-    table_name  = "carbonintensities"
-    table_client = TableServiceClient.from_connection_string(DEPLOYMENT_STORAGE_CONNECTION_STRING).get_table_client(table_name)
+    # table_name  = "carbonintensities"
+    # table_client = TableServiceClient.from_connection_string(DEPLOYMENT_STORAGE_CONNECTION_STRING).get_table_client(table_name)
 
-    data = {
-        "PartitionKey": "pending", # Effectively table name
-        "RowKey": str(uuid.uuid4()), # Generates a key 
-        "Zone": cur_zone,
-        "CI": cur_CI,
-        "CI_TS": timestamp
-    }
+    # data = {
+    #     "PartitionKey": timestamp, # Effectively table name
+    #     "RowKey": str(uuid.uuid4()), # Generates a key 
+    #     "Zone": cur_zone,
+    #     "CI": cur_CI,
+    # }
 
-    table_client.create_entity(entity=data)
+    # table_client.create_entity(entity=data)
 
     return func.HttpResponse(
             json.dumps(rows),
--- TimerTrigger/__init__.py
+++ TimerTrigger/__init__.py
@@ -1,13 +1,58 @@
-import datetime
 import logging
-
+import json
 import azure.functions as func
-
+from azure.data.tables import TableServiceClient
+import os
+from utils import get_cur_CI
+import uuid
 
 def main(mytimer: func.TimerRequest) -> None:
-    utc_timestamp = datetime.now().isoformat()
+    AZURE_OPENAI_ENDPOINT = os.environ.get("AZURE_OPENAI_ENDPOINT")
+    AZURE_OPENAI_KEY = os.environ.get("AZURE_OPENAI_KEY")
+    DEPLOYMENT_STORAGE_CONNECTION_STRING = os.environ.get("DEPLOYMENT_STORAGE_CONNECTION_STRING")
+    ELECTRICITY_MAPS_API_KEY = os.environ.get("ELECTRICITY_MAPS_API_KEY")
 
-    if mytimer.past_due:
-        logging.info('The timer is past due!')
+    if not AZURE_OPENAI_ENDPOINT:
+        return func.HttpResponse(
+            body=json.dumps({"error": "AZ_OPENAI_ENDPOINT not configured"}),
+            status_code=500,
+            mimetype="application/json"
+        )
+    
+    if not AZURE_OPENAI_KEY:
+        return func.HttpResponse(
+            body=json.dumps({"error": "AZ_OPENAI_KEY not configured"}),
+            status_code=500,
+            mimetype="application/json"
+        )
+    
+    if not DEPLOYMENT_STORAGE_CONNECTION_STRING:
+        return func.HttpResponse(
+            body=json.dumps({"error": "DEPLOYMENT_STORAGE_CONNECTION_STRING not configured"}),
+            status_code=500,
+            mimetype="application/json"
+        )
+    
+    if not ELECTRICITY_MAPS_API_KEY:
+        return func.HttpResponse(
+            body=json.dumps({"error": "ELECTRICITY_MAPS_API_KEY not configured"}),
+            status_code=500,
+            mimetype="application/json"
+        )
 
-    logging.info('Python timer trigger function ran at %s', utc_timestamp)
+    # Lastly, store the CI in a table
+    cur_CI, cur_zone, timestamp = get_cur_CI(ELECTRICITY_MAPS_API_KEY)
+
+    table_name  = "carbonintensities"
+    table_client = TableServiceClient.from_connection_string(DEPLOYMENT_STORAGE_CONNECTION_STRING).get_table_client(table_name)
+
+    data = {
+        "PartitionKey": timestamp, # Effectively table name
+        "RowKey": str(uuid.uuid4()), # Generates a key 
+        "Zone": cur_zone,
+        "CI": cur_CI,
+    }
+
+    table_client.create_entity(entity=data)
+
+    logging.info('Python timer trigger executed successfully.')
