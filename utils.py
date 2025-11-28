e309d3e35db7868b627ff067be03c910410a21d7
ArtyNar <arty.nar00@gmail.com>
2025-11-27 20:24:56 -0700

    Remove later

--- utils.py
+++ utils.py
@@ -58,7 +58,7 @@ def get_bin(ci_old, cur_CI, DEPLOYMENT_STORAGE_CONNECTION_STRING):
 
     entities = table_client.query_entities(query)
     rows = [dict(e) for e in entities]
-    CIs = [row['CI'] for row in rows]
+    CIs = [float(row['CI']) for row in rows]
     min_ci = min(CIs)
     max_ci = max(CIs)
 
