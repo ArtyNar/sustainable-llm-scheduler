5c18dfa9378bdfc6e12ad034ebcfa2352d847c38
ArtyNar <arty.nar00@gmail.com>
2025-11-28 21:53:54 -0700

    Fix fields

--- HttpTrigger1/__init__.py
+++ HttpTrigger1/__init__.py
@@ -95,21 +95,7 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
             mimetype="application/json"
         )   
     
-    table_name  = "carbonintensities"
-    table_client = TableServiceClient.from_connection_string(DEPLOYMENT_STORAGE_CONNECTION_STRING).get_table_client(table_name)
-    
-    cutoff = datetime.now(timezone.utc) - timedelta(days=7)
-    cutoff_str = cutoff.isoformat().replace("+00:00", "Z")
-    
-    query = (
-        f"PartitionKey eq 'ci' and "
-        f"Timestamp ge datetime'{cutoff_str}'"
-    )
-    
-    entities = table_client.query_entities(query)
-    rows = [dict(e) for e in entities]
-
     return func.HttpResponse(
-            json.dumps(rows),
+            json.dumps({"success": f"Everything went well"}),
             status_code=200
     )
