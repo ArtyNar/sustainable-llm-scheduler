6ca6dc242abfdc833f4cc61f42840cb74fa1be63
ArtyNar <arty.nar00@gmail.com>
2025-11-30 12:15:11 -0700

    Test new scheduler

--- HttpTrigger1/__init__.py
+++ HttpTrigger1/__init__.py
@@ -3,7 +3,7 @@ import json
 import azure.functions as func
 from azure.data.tables import TableServiceClient
 import os
-from utils import get_cur_CI, get_bin, execute, get_execution_probability
+from utils import get_cur_CI, get_bin, execute, get_execution_probability, get_ci_history
 import uuid
 from datetime import datetime, timezone
 import random
@@ -46,56 +46,59 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
             mimetype="application/json"
         )
     
-    # Get latest carbon intensity
-    cur_CI, _, _ = get_cur_CI(ELECTRICITY_MAPS_API_KEY)
+    # # Get latest carbon intensity
+    # cur_CI, cur_zone, _ = get_cur_CI(ELECTRICITY_MAPS_API_KEY)
+    
+    # # Get last weeks history for binning and rate of change information
+    # CIs_all, CIs_latest = get_ci_history(DEPLOYMENT_STORAGE_CONNECTION_STRING)
 
-    table_name  = "prompttable"
-    table_client = TableServiceClient.from_connection_string(DEPLOYMENT_STORAGE_CONNECTION_STRING).get_table_client(table_name)
+    # # Access prompt table
+    # try:
+    #     table_name  = "prompttable"
+    #     table_client = TableServiceClient.from_connection_string(DEPLOYMENT_STORAGE_CONNECTION_STRING).get_table_client(table_name)
 
-    entities = table_client.query_entities(
-        query_filter="Status eq 'pending'"
-    )
+    #     entities = table_client.query_entities(
+    #         query_filter="Status eq 'pending'" # Filter on pending only
+    #     )
 
-    try:
-        for entity in entities:
-            model = entity["Model"]
-            prompt_text = entity["Prompt"]
-            expirationDate = datetime.fromisoformat(entity['expirationDate'])
-            CI_old = entity['CarbonIntensity_s'].value
-            now = datetime.now(timezone.utc)
+    # except Exception as e:
+    #     logging.error(f"Something went wrong accessing the prompt table: {e}")
+    
+    # try:
+    #     for entity in entities:
+    #         model = entity["Model"]
+    #         prompt_text = entity["Prompt"]
+    #         expirationDate = datetime.fromisoformat(entity['expirationDate'])
+    #         CI_old = entity['CarbonIntensity_s'].value
+    #         now = datetime.now(timezone.utc)
             
-            logging.info(f'Processing: {prompt_text}')
+    #         # Time remaining
+    #         delta = expirationDate - now
+    #         remaining_hours = delta.total_seconds() / 3600
 
-            # Time remaining
-            delta = expirationDate - now
-            remaining_hours = delta.total_seconds() / 3600
+    #         # Get bins for current and past CI (0-5)
+    #         bin_old, bin_new = get_bin(CI_old, cur_CI, CIs_all)
 
-            # Get bins for current and past CI (0-5)
-            bin_old, bin_new, CIs = get_bin(CI_old, cur_CI, DEPLOYMENT_STORAGE_CONNECTION_STRING)
+    #         # If scheduler failed to execute in time, execute 
+    #         if now > expirationDate:
+    #             logging.info('Prompt expired.')
+    #             execute(entity, cur_CI, table_client, model, prompt_text, AZURE_OPENAI_ENDPOINT, AZURE_OPENAI_KEY, -1)
+    #             continue
+    #         # If carbon intensity is very low, execute
+    #         elif bin_new == 0 or bin_new == 1: 
+    #             execute(entity, cur_CI, table_client, model, prompt_text, AZURE_OPENAI_ENDPOINT, AZURE_OPENAI_KEY, bin_new)
+            
+    #         else:
+    #             prob = get_execution_probability(bin_old, bin_new, CIs_latest, remaining_hours)
+    #             r = random.random()
+    #             if r < prob:
+    #                 logging.info(f'Rand: {r}')
 
-            # If scheduler failed to execute in time, execute 
-            if now > expirationDate:
-                logging.info('Prompt expired.')
-                execute(entity, cur_CI, table_client, model, prompt_text, AZURE_OPENAI_ENDPOINT, AZURE_OPENAI_KEY, 10000)
-                continue
-            # If carbon intensity is very low, execute
-            elif bin_new == 0: 
-                execute(entity, cur_CI, table_client, model, prompt_text, AZURE_OPENAI_ENDPOINT, AZURE_OPENAI_KEY, bin_new)
-            else:
-                prob = get_execution_probability(bin_old, bin_new, CIs, remaining_hours)
-                r = random.random()
-                if r < prob:
-                    logging.info(f'Rand: {r}')
-
-                    execute(entity, cur_CI, table_client, model, prompt_text, AZURE_OPENAI_ENDPOINT, AZURE_OPENAI_KEY, prob)
+    #                 execute(entity, cur_CI, table_client, model, prompt_text, AZURE_OPENAI_ENDPOINT, AZURE_OPENAI_KEY, prob)
                 
-    except Exception as e:
-        logging.error(f"Something went wrong: {e}")
-        return func.HttpResponse(
-            body=json.dumps({"error": f"Something went wrong: {str(e)}"}),
-            status_code=500,
-            mimetype="application/json"
-        )   
+    # except Exception as e:
+    #     logging.error(f"Something went wrong with the scheduler: {e}")
+
 
     # table_name  = "prompttable"
     # table_client = TableServiceClient.from_connection_string(DEPLOYMENT_STORAGE_CONNECTION_STRING).get_table_client(table_name)
